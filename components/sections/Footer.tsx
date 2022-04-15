b53189d14532cea2dd85785735a1f58f8bff0c31
alastairblackwood <agrizzlybear@icloud.com>
2022-04-15 10:05:59 +1000

    added external tab opening for social links

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,16 +1,14 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
 import {
   chakra,
-  Box,
   Container,
   Flex,
   Text,
   Stack,
   useColorModeValue,
   VisuallyHidden,
-  VStack,
-  Center,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 import {
   FaApple,
   FaSpotify,
@@ -20,7 +18,7 @@ import {
   FaInstagram,
   FaTwitter,
   FaSoundcloud,
-} from "react-icons/fa";
+} from 'react-icons/fa';
 
 const SocialButton = ({
   children,
@@ -33,7 +31,7 @@ const SocialButton = ({
 }) => {
   return (
     <chakra.button
-      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
       rounded="full"
       w={8}
       h={8}
@@ -45,7 +43,7 @@ const SocialButton = ({
       justifyContent="center"
       transition="background 0.3s ease"
       _hover={{
-        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
       }}
     >
       <VisuallyHidden>{label}</VisuallyHidden>
@@ -60,59 +58,129 @@ const Footer = () => {
       as={Stack}
       maxW="6xl"
       py={4}
-      direction={{ base: "column", md: "column" }}
+      direction={{ base: 'column', md: 'column' }}
       spacing={4}
-      justify={{ base: "center", md: "space-between" }}
-      align={{ base: "center", md: "center" }}
+      justify={{ base: 'center', md: 'space-between' }}
+      align={{ base: 'center', md: 'center' }}
     >
       <Stack direction="row" spacing={10} mb={2}>
+
+         {/* APPLE MUSIC */}
         <SocialButton
           label="Apple"
           href={
-            "https://music.apple.com/us/artist/motion-picture-soundtrack/163261601"
+            'https://music.apple.com/us/artist/motion-picture-soundtrack/163261601'
           }
         >
-          <FaApple size="10rem" />
+          <Link
+            href={
+              'https://music.apple.com/us/artist/motion-picture-soundtrack/163261601'
+            }
+          >
+            <a target="_blank">
+              <FaApple size="3rem" />
+            </a>
+          </Link>
         </SocialButton>
+
+        {/* SPOTIFY */}
         <SocialButton
           label="Spotify"
           href={
-            "https://open.spotify.com/artist/0hK5VUQ7uhgezxt2YCqiB0?si=fxMGL1apTI-1TM1XSztQow"
+            'https://open.spotify.com/artist/0hK5VUQ7uhgezxt2YCqiB0?si=fxMGL1apTI-1TM1XSztQow'
           }
         >
-          <FaSpotify size="10rem" />
+          <Link
+            href={
+              'https://open.spotify.com/artist/0hK5VUQ7uhgezxt2YCqiB0?si=fxMGL1apTI-1TM1XSztQow'
+            }
+          >
+            <a target="_blank">
+              <FaSpotify size="2.5rem" />
+            </a>
+          </Link>
         </SocialButton>
+
+         {/* YOUTUBE */}
         <SocialButton
           label="YouTube"
           href={
-            "https://music.youtube.com/channel/UCTy5qUNP5bk-NckveoZts9A?feature=share"
+            'https://music.youtube.com/channel/UCTy5qUNP5bk-NckveoZts9A?feature=share'
           }
         >
-          <FaYoutube size="10rem" />
+          <Link
+            href={
+              'https://music.youtube.com/channel/UCTy5qUNP5bk-NckveoZts9A?feature=share'
+            }
+          >
+            <a target="_blank">
+              <FaYoutube size="3rem" />
+            </a>
+          </Link>
         </SocialButton>
+
+         {/* SOUNDCLOUD */}
         <SocialButton
           label="SoundCloud"
-          href={"https://soundcloud.com/motionpicturesoundtrack"}
+          href={'https://soundcloud.com/motionpicturesoundtrack'}
         >
-          <FaSoundcloud size="10rem" />
+           <Link
+            href={
+              'https://soundcloud.com/motionpicturesoundtrac
+            }
+          >
+            <a target="_blank">
+            <FaSoundcloud size="3rem" />
+            </a>
+          </Link>
         </SocialButton>
+
+         {/* FACEBOOK */}
         <SocialButton
           label="Facebook"
-          href={"https://www.facebook.com/motionpicturesoundtrack"}
+          href={'https://www.facebook.com/motionpicturesoundtrack'}
         >
-          <FaFacebook size="10rem" />
+          <Link
+            href={
+              'https://www.facebook.com/motionpicturesoundtrack'
+            }
+          >
+            <a target="_blank">
+            <FaFacebook size="3rem" />
+            </a>
+          </Link> 
         </SocialButton>
+
+         {/* INSTAGRAM */}
         <SocialButton
           label="Instagram"
-          href={"https://www.instagram.com/motionpix/?hl=en"}
+          href={'https://www.instagram.com/motionpix/?hl=en'}
         >
-          <FaInstagram size="10rem" />
+            <Link
+            href={
+              'https://www.instagram.com/motionpix/?hl=en'
+            }
+          >
+            <a target="_blank">
+            <FaInstagram size="3rem" />
+            </a>
+          </Link>
         </SocialButton>
+
+         {/* TWITTER */}
         <SocialButton
-          label={"Twitter"}
-          href={"https://twitter.com/mpsofficial"}
+          label={'Twitter'}
+          href={'https://twitter.com/mpsofficial'}
         >
-          <FaTwitter size="5rem" />
+           <Link
+            href={
+              'https://twitter.com/mpsofficial'
+            }
+          >
+            <a target="_blank">
+            <FaTwitter size="3rem" />
+            </a>
+          </Link>
         </SocialButton>
       </Stack>
 
